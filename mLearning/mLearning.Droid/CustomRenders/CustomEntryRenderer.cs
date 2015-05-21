52cb4ff0ef157b186de063ff6823d58644756b65
milton761 <milton761@gmail.com>
2015-05-21 11:36:22 -0500

    Revert "Revert "Upload""
    This reverts commit 85b1451d7667296ba7638eedf225b5f8718c6dd3.

---
+++ mLearning/mLearning.Droid/CustomRenders/CustomEntryRenderer.cs
@@ -0,0 +1,29 @@
+﻿using mLearning;
+using mLearning.CustomRenders;
+using mLearning.Droid;
+using mLearning.Droid.CustomRenders;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+
+namespace mLearning.Droid.CustomRenders
+{
+
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        // Override the OnElementChanged method so we can tweak this renderer post-initial setup
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Entry> e)
+        {
+
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                // do whatever you want to the textField here!
+                Control.SetBackgroundColor(global::Android.Graphics.Color.Transparent);
+            }
+        }
+    }
+}
---
+++ mLearning/mLearning/Views/LoginPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace mLearning.Views
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnTapGestureRecognizerEnterButton(object sender, EventArgs args)
+        {
+            await Navigation.PushModalAsync(new MasterDetailMainPage());
+        }
+
+        async void OnButtonClicked_SignUp(object sender, EventArgs args)
+        {
+            await Navigation.PushModalAsync(new SignUpPage());
+        }
+    }
+}
