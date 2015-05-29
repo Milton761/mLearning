eaea5a4078ca47b9fba436035e6a10cbb121a296
milton761 <milton761@gmail.com>
2015-05-29 16:30:04 -0500

    add var names in XAML ConnectLoginPage

--- mLearning/mLearning/Views/ConnectLoginPage.xaml.cs
+++ mLearning/mLearning/Views/ConnectLoginPage.xaml.cs
@@ -10,13 +10,35 @@ namespace mLearning.Views
 {
     public partial class ConnectLoginPage : ContentPage
     {
+        
         public ConnectLoginPage()
         {
             InitializeComponent();
-
-            
-
         }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            this.Animate("",new Animation(),0,0, Easing.Linear, null, null);
+            _LogoEdutic.RotateTo(90);
+            
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            this.Animate("",new Animation(),0,0, Easing.Linear, null, null);
+            _LogoEdutic.RotateTo(-90);
+            
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            base.OnBackButtonPressed();
+            this.Animate("", new Animation(), 0, 0, Easing.Linear, null, null);
+            return true;
+        }
+
 
         async void OnButtonClicked_Login(object sender, EventArgs args)
         {
